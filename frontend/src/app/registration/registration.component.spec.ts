3b4d6818e49c513cec5b15d485ea21957cd3fee3
IlinaGergova <ili.gergova@gmail.com>
2025-08-11 15:50:18 +0300

    validate registration form fields
    * add tests

--- frontend/src/app/registration/registration.component.spec.ts
+++ frontend/src/app/registration/registration.component.spec.ts
@@ -1,22 +1,119 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegistrationComponent } from './registration.component';
+import { provideHttpClient } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { UsersService } from '../users.service';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { By } from '@angular/platform-browser';
+
+class UsersServiceMock {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public registerUser(email: string, password: string): Observable<object> {
+    return of({});
+  }
+}
 
 describe('RegistrationComponent', () => {
   let component: RegistrationComponent;
   let fixture: ComponentFixture<RegistrationComponent>;
+  const usersServiceMock = new UsersServiceMock();
+  let emailInput: HTMLInputElement;
+  let passwordInput: HTMLInputElement;
+  let templateRef: HTMLElement;
 
-  beforeEach(async() => {
-    await TestBed.configureTestingModule({
-      imports: [RegistrationComponent]
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [RegistrationComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
     }).compileComponents();
+
+    TestBed.overrideProvider(UsersService, {useValue: usersServiceMock});
 
     fixture = TestBed.createComponent(RegistrationComponent);
     component = fixture.componentInstance;
+
+    templateRef = fixture.debugElement.nativeElement as HTMLElement;
+    emailInput = templateRef.querySelector('#email') as HTMLInputElement;
+    passwordInput = templateRef.querySelector('#password') as HTMLInputElement;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should successfully register user', () => {
+    emailInput.value = 'mockEmail@email.com';
+    passwordInput.value = 'mockPassword';
+    const registerSpy = jest.spyOn(usersServiceMock, 'registerUser');
+
+    component.register();
+    expect(registerSpy).toHaveBeenCalledWith('mockEmail@email.com', 'mockPassword');
+    expect(component.responseMessage).toBe('Registration successful!');
+    expect(emailInput.value).toBe('');
+    expect(passwordInput.value).toBe('');
+  });
+
+  it('should show error message when user registration is failed', () => {
+    emailInput.value = 'mockEmail@email.com';
+    passwordInput.value = 'mockPassword';
+    const registerSpy = jest.spyOn(usersServiceMock, 'registerUser')
+      .mockReturnValue(throwError(() => new Error()));
+
+    component.register();
+    expect(registerSpy).toHaveBeenCalledWith('mockEmail@email.com', 'mockPassword');
+    expect(component.responseMessage).toBe('Registration failed!');
+    expect(emailInput.value).toBe('mockEmail@email.com');
+    expect(passwordInput.value).toBe('mockPassword');
+  });
+
+  it('should show error message when email is in invalid', () => {
+    emailInput.value = 'mockInvalidEmail';
+    const registerSpy = jest.spyOn(usersServiceMock, 'registerUser');
+
+    component.register();
+    expect(registerSpy).not.toHaveBeenCalledWith();
+    expect(component.validationMessage).toBe('Invalid email format.');
+    expect(emailInput.value).toBe('mockInvalidEmail');
+  });
+
+  it('should show error message when password is in invalid', () => {
+    emailInput.value = 'mockEmail@email.com';
+    passwordInput.value = '12';
+    const registerSpy = jest.spyOn(usersServiceMock, 'registerUser');
+
+    component.register();
+    expect(registerSpy).not.toHaveBeenCalledWith();
+    expect(component.validationMessage).toBe('Password must be at least 6 characters long.');
+    expect(emailInput.value).toBe('mockEmail@email.com');
+    expect(passwordInput.value).toBe('12');
+  });
+
+  it('should focus email input when trying to create user without email', () => {
+    emailInput.value = '';
+    const registerSpy = jest.spyOn(usersServiceMock, 'registerUser');
+
+    component.register();
+    expect(registerSpy).not.toHaveBeenCalledWith();
+    const focusedElement = fixture.debugElement.query(By.css(':focus')).nativeElement as HTMLInputElement;
+    expect(focusedElement).toBe(emailInput);
+    expect(component.validationMessage).toBe('');
+  });
+
+  it('should focus password input when trying to create user without password', () => {
+    emailInput.value = 'mockEmail@email.com';
+    passwordInput.value = '';
+    const registerSpy = jest.spyOn(usersServiceMock, 'registerUser');
+
+    component.register();
+    expect(registerSpy).not.toHaveBeenCalledWith();
+    const focusedElement = fixture.debugElement.query(By.css(':focus')).nativeElement as HTMLInputElement;
+    expect(focusedElement).toBe(passwordInput);
+    expect(component.validationMessage).toBe('');
   });
 });
--- frontend/src/app/registration/registration.component.ts
+++ frontend/src/app/registration/registration.component.ts
@@ -1,28 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { UsersService } from '../users.service';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-registration',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css',
   providers: [UsersService]
 })
 export class RegistrationComponent {
-  public email: string = '';
-  public password: string = '';
+  @ViewChild('emailInput') private email!: ElementRef;
+  @ViewChild('passwordInput') private password!: ElementRef;
+  public responseMessage: string = '';
+  public validationMessage: string = '';
 
   public constructor(private usersService: UsersService) {}
 
   public register(): void {
-    this.usersService.registerUser(this.email, this.password).subscribe({
-      next: (response) => {
-        console.log('Registration successful', response);
+    this.responseMessage = '';
+    if (!this.isFormValid()) {
+      return;
+    }
+    const email = (this.email.nativeElement as HTMLInputElement).value;
+    const password = (this.password.nativeElement as HTMLInputElement).value;
+    this.usersService.registerUser(email, password).subscribe({
+      next: () => {
+        this.responseMessage = 'Registration successful!';
+        this.cleanInputs();
       },
-      error: (error) => {
-        console.error('Registration failed', error);
+      error: () => {
+        this.responseMessage = 'Registration failed!';
       }
     });
+  }
+
+  private validateEmail(): boolean {
+    const email = (this.email.nativeElement as HTMLInputElement).value;
+    const re = new RegExp(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/);
+    return re.test(String(email).toLowerCase());
+  }
+
+  private validatePassword(): boolean {
+    const password = (this.password.nativeElement as HTMLInputElement).value;
+    const re = new RegExp(/.{6,}/);
+    return re.test(String(password).toLowerCase());
+  }
+
+  private isFormValid(): boolean {
+    const emailRef = this.email.nativeElement as HTMLInputElement;
+    const passwordRef = this.password.nativeElement as HTMLInputElement;
+    this.validationMessage = '';
+    if (!emailRef.value) {
+      emailRef.focus();
+      return false;
+    }
+    if (!this.validateEmail()) {
+      emailRef.focus();
+      this.validationMessage = 'Invalid email format.';
+      return false;
+    }
+    if (!passwordRef.value) {
+      passwordRef.focus();
+      return false;
+    }
+    if (!this.validatePassword()) {
+      passwordRef.focus();
+      this.validationMessage = 'Password must be at least 6 characters long.';
+      return false;
+    }
+    return true;
+  }
+
+  private cleanInputs(): void {
+    (this.email.nativeElement as HTMLInputElement).value = '';
+    (this.password.nativeElement as HTMLInputElement).value = '';
   }
 }
