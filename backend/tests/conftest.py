68d75659d2475ef2b366cc3f6a5092c77393aa4c
iordanivanov <iiordan.ivanov@gmail.com>
2025-08-12 12:35:24 +0300

    Split up backend unit tests code into separate files

---
+++ backend/tests/conftest.py
@@ -0,0 +1,76 @@
+import pathlib
+import shutil
+import pytest
+from django.test import Client
+from django.conf import settings
+
+from web_annotation.models import Job, User
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_data_dirs():
+    assert not pathlib.Path(settings.DATA_STORAGE_DIR).exists()
+    pathlib.Path(settings.DATA_STORAGE_DIR).mkdir()
+    pathlib.Path(settings.ANNOTATION_CONFIG_STORAGE_DIR).mkdir()
+    pathlib.Path(settings.JOB_INPUT_STORAGE_DIR).mkdir()
+    pathlib.Path(settings.JOB_RESULT_STORAGE_DIR).mkdir()
+    yield
+    shutil.rmtree(settings.DATA_STORAGE_DIR)
+
+
+@pytest.fixture(autouse=True)
+def setup_test_db(
+    db,
+    tmp_path: pathlib.Path,
+) -> None:
+    user = User.objects.create_user(
+        "test-user",
+        "user@example.com",
+        "secret",
+    )
+    user.save()
+    user_input = tmp_path / "user-input.vcf"
+    user_input.write_text("mock vcf data")
+    user_config = tmp_path / "user-config.yaml"
+    user_config.write_text("mock annotation config")
+    user_result = tmp_path / "user-result.vcf"
+    user_result.write_text("mock annotated vcf")
+    Job(
+        input_path=user_input,
+        config_path=user_config,
+        result_path=user_result,
+        owner=user,
+    ).save()
+
+    admin = User.objects.create_superuser(
+        "test-admin",
+        "admin@example.com",
+        "secret",
+    )
+    admin.save()
+    admin_input = tmp_path / "admin-input.vcf"
+    admin_input.write_text("mock vcf data 2")
+    admin_config = tmp_path / "admin-config.yaml"
+    admin_config.write_text("mock annotation config 2")
+    admin_result = tmp_path / "admin-result.vcf"
+    admin_result.write_text("mock annotated vcf 2")
+    Job(
+        input_path=admin_input,
+        config_path=admin_config,
+        result_path=admin_result,
+        owner=admin,
+    ).save()
+
+
+@pytest.fixture
+def admin_client() -> Client:
+    client = Client()
+    client.login(email="admin@example.com", password="secret")
+    return client
+
+
+@pytest.fixture
+def user_client() -> Client:
+    client = Client()
+    client.login(email="user@example.com", password="secret")
+    return client
---
+++ backend/tests/test_users_api.py
@@ -0,0 +1,117 @@
+from django.test import Client
+
+from web_annotation.models import User
+
+
+def test_get_users(admin_client: Client) -> None:
+    response = admin_client.get("/users/")
+    assert response.status_code == 200
+    assert response.json() == [
+        {"email": "user@example.com", "jobs": [1]},
+        {"email": "admin@example.com", "jobs": [2]},
+    ]
+
+
+def test_get_users_unauthorized(user_client: Client) -> None:
+    response = user_client.get("/users/")
+    assert response.status_code == 403
+
+
+def test_get_user_details(admin_client: Client) -> None:
+    response = admin_client.get("/users/1/")
+    assert response.status_code == 200
+    assert response.json() == {"email": "user@example.com", "jobs": [1]}
+
+
+def test_get_user_details_unauthorized(user_client: Client) -> None:
+    response = user_client.get("/users/1/")
+    assert response.status_code == 403
+
+
+def test_register(client: Client) -> None:
+    response = client.post(
+        "/register/",
+        {"email": "gosho@example.com",
+         "password": "secret"},
+        content_type="application/json",
+    )
+    assert response.status_code == 200
+    assert User.objects.filter(email="gosho@example.com").exists()
+
+
+def test_register_email_taken(client: Client) -> None:
+    response = client.post(
+        "/register/",
+        {"email": "user@example.com",
+         "password": "secret"},
+        content_type="application/json",
+    )
+    assert response.status_code == 400
+
+
+def test_register_bad_requests(client: Client) -> None:
+    response = client.post(
+        "/register/",
+        {"email": "gosho@example.com"},
+        content_type="application/json",
+    )
+    assert response.status_code == 400
+
+    response = client.post(
+        "/register/",
+        {"password": "secret"},
+        content_type="application/json",
+    )
+    assert response.status_code == 400
+
+
+def test_login(client: Client) -> None:
+    response = client.post(
+        "/login/",
+        {"email": "user@example.com",
+         "password": "secret"},
+        content_type="application/json",
+    )
+    assert response.status_code == 200
+
+    assert "sessionid" in response.cookies
+    assert response.cookies["sessionid"]
+
+    assert "csrftoken" in response.cookies
+    assert response.cookies["csrftoken"]
+
+
+def test_login_user_wrong_password(client: Client) -> None:
+    response = client.post(
+        "/login/",
+        {"email": "user@example.com",
+         "password": "alabala"},
+        content_type="application/json",
+    )
+    assert response.status_code == 400
+
+
+def test_login_user_does_not_exist(client: Client) -> None:
+    response = client.post(
+        "/login/",
+        {"email": "user-two@example.com",
+         "password": "secret"},
+        content_type="application/json",
+    )
+    assert response.status_code == 400
+
+
+def test_login_bad_requests(client: Client) -> None:
+    response = client.post(
+        "/login/",
+        {"email": "user@example.com"},
+        content_type="application/json",
+    )
+    assert response.status_code == 400
+
+    response = client.post(
+        "/login/",
+        {"password": "secret"},
+        content_type="application/json",
+    )
+    assert response.status_code == 400
