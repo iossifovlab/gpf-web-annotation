ac8471f59b95983e0d1014e5a33fcb9bb21e7c3c
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2025-09-09 18:17:03 +0300

    Experiment with building GPFWA frontend

---
+++ frontend/scripts/supervisord-bootstrap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+
+a2enmod headers
+
+echo "enabling apache site: localhost..."
+a2ensite localhost
+
+supervisorctl start apache2
+
+/wait-for-it.sh localhost:80 -t 240
+
+rc=$?
+if [ $rc -ne 0 ]; then
+    echo -e "\n---------------------------------------"
+    echo -e "  Apache2 not ready! Exiting..."
+    echo -e "---------------------------------------"
+    exit 1
+fi
+
+echo -e "\n\n--------------------------------------------------------------------------------"
+echo -e "Apache2 running..."
+echo -e "--------------------------------------------------------------------------------\n\n"
