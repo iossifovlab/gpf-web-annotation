df959f09a40cde0507765d561aac88d01228cd36
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2025-09-11 10:19:14 +0300

    Try to handle linter reports paths to source files

--- backend/scripts/backend_linters.sh
+++ backend/scripts/backend_linters.sh
@@ -2,18 +2,19 @@
 
 /opt/conda/bin/conda run --no-capture-output -n gpf pip install -e /wd/backend
 
-cd /wd/backend/
+cd /wd/
 
 /opt/conda/bin/conda run --no-capture-output -n gpf ruff check \
     --exit-zero \
     --output-format=pylint \
-    --output-file=/wd/results/ruff_report web_annotation || true
+    --output-file=/wd/results/ruff_report backend/web_annotation || true
 
 /opt/conda/bin/conda run --no-capture-output -n gpf \
-    pylint web_annotation -f parseable --reports=no -j 4 \
+    pylint backend/web_annotation -f parseable --reports=no -j 4 \
     --exit-zero > /wd/results/pylint_report || true
 
-/opt/conda/bin/conda run --no-capture-output -n gpf mypy web_annotation \
+/opt/conda/bin/conda run --no-capture-output -n gpf mypy \
+    backend/web_annotation \
     --pretty \
     --show-error-context \
     --no-incremental \
