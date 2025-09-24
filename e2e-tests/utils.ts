ba513482cf6fa7183756d64429dd127ddfcdb9a0
IlinaGergova <ili.gergova@gmail.com>
2025-09-24 12:54:52 +0300

    remove frontend url in utils

--- e2e-tests/utils.ts
+++ e2e-tests/utils.ts
@@ -1,14 +1,11 @@
 import { expect, Page } from '@playwright/test';
 
-export const backendUrl = 'http://localhost:8000';
-export const frontendUrl = 'http://frontend';
-
 export function getRandomString(): string {
   return Math.random().toString(36).substring(2, 9);
 }
 
 export async function registerUser(page: Page, email: string, password: string): Promise<void> {
-  await page.goto(frontendUrl + '/register', {waitUntil: 'load'});
+  await page.goto('/register', {waitUntil: 'load'});
   await page.locator('#email').pressSequentially(email);
   await page.locator('#password').pressSequentially(password);
   await page.getByRole('button', { name: 'Create' }).click();
@@ -16,7 +13,7 @@ export async function registerUser(page: Page, email: string, password: string):
 }
 
 export async function loginUser(page: Page, email: string, password: string): Promise<void> {
-  await page.goto(frontendUrl + '/login', {waitUntil: 'load'});
+  await page.goto('/login', {waitUntil: 'load'});
   await page.locator('#email').pressSequentially(email);
   await page.locator('#password').pressSequentially(password);
   await page.getByRole('button', { name: 'Login' }).click();
