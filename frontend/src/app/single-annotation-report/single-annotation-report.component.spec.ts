2206d67bf0ae0f5786e351e187e5816a47e08727
IlinaGergova <ili.gergova@gmail.com>
2025-09-29 16:14:21 +0300

    make request for report in annotation component
    * add unit test

--- frontend/src/app/single-annotation-report/single-annotation-report.component.spec.ts
+++ frontend/src/app/single-annotation-report/single-annotation-report.component.spec.ts
@@ -1,14 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SingleAnnotationReportComponent } from './single-annotation-report.component';
+import { Observable, of } from 'rxjs';
+import { Annotator, AnnotatorType, SingleAnnotationReport, Variant } from '../single-annotation';
+import { SingleAnnotationService } from '../single-annotation.service';
 
+const mockReport = new SingleAnnotationReport(
+  new Variant('chr14', '204000100', 'A', 'AA', 'ins'),
+  [
+    new Annotator({} as AnnotatorType, [])
+  ],
+);
+class MockSingleAnnotationService {
+  public getReport(): Observable<SingleAnnotationReport> {
+    return of(mockReport);
+  }
+}
 describe('SingleAnnotationReportComponent', () => {
   let component: SingleAnnotationReportComponent;
   let fixture: ComponentFixture<SingleAnnotationReportComponent>;
+  const mockSingleAnnotationService = new MockSingleAnnotationService();
 
   beforeEach(async() => {
     await TestBed.configureTestingModule({
-      imports: [SingleAnnotationReportComponent]
+      imports: [SingleAnnotationReportComponent],
+      providers: [
+        {
+          provide: SingleAnnotationService,
+          useValue: mockSingleAnnotationService
+        }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(SingleAnnotationReportComponent);
@@ -18,5 +39,10 @@ describe('SingleAnnotationReportComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get report from service', () => {
+    component.ngOnInit();
+    expect(component.report).toBe(mockReport);
   });
 });
--- frontend/src/app/single-annotation-report/single-annotation-report.component.ts
+++ frontend/src/app/single-annotation-report/single-annotation-report.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SingleAnnotationService } from '../single-annotation.service';
+import { SingleAnnotationReport } from '../single-annotation';
+import { CommonModule } from '@angular/common';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-single-annotation-report',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './single-annotation-report.component.html',
   styleUrl: './single-annotation-report.component.css'
 })
-export class SingleAnnotationReportComponent {
+export class SingleAnnotationReportComponent implements OnInit {
+  public report: SingleAnnotationReport = null;
 
+  public constructor(private singleAnnotationService: SingleAnnotationService) { }
+
+  public ngOnInit(): void {
+    this.singleAnnotationService.getReport().pipe(take(1)).subscribe(report => {
+      this.report = report;
+    });
+  }
 }
