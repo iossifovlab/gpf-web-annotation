765737eca4d47fa4510227aea79ac4e1247bb0dd
IlinaGergova <ili.gergova@gmail.com>
2025-09-30 14:16:57 +0300

    implement single annotation component
    * get genomes on component initialization
    * redirect to report page when clicking go button
    and pass variant and genome as params

--- frontend/src/app/single-annotation/single-annotation.component.spec.ts
+++ frontend/src/app/single-annotation/single-annotation.component.spec.ts
@@ -1,15 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SingleAnnotationComponent } from './single-annotation.component';
+import { Observable, of } from 'rxjs';
+import { SingleAnnotationService } from '../single-annotation.service';
+import { provideRouter } from '@angular/router';
+
+class SingleAnnotationServiceMock {
+  public getGenomes(): Observable<string[]> {
+    return of(['hg38', 'hg19']);
+  }
+}
 
 describe('SingleAnnotationComponent', () => {
   let component: SingleAnnotationComponent;
   let fixture: ComponentFixture<SingleAnnotationComponent>;
+  const singleAnnotationService = new SingleAnnotationServiceMock();
 
   beforeEach(async() => {
     await TestBed.configureTestingModule({
-      imports: [SingleAnnotationComponent]
+      imports: [SingleAnnotationComponent],
+      providers: [
+        provideRouter([]),
+      ],
     }).compileComponents();
+
+    TestBed.overrideProvider(SingleAnnotationService, {useValue: singleAnnotationService});
 
     fixture = TestBed.createComponent(SingleAnnotationComponent);
     component = fixture.componentInstance;
@@ -18,5 +33,24 @@ describe('SingleAnnotationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get genomes on component initialization', () => {
+    component.ngOnInit();
+    expect(component.genomes).toStrictEqual(['hg38', 'hg19']);
+    expect(component.selectedGenome).toBe('hg38');
+  });
+
+  it('should navigate to report page with and pass parameters', () => {
+    component.selectedGenome = 'hg19';
+    const routerSpy = jest.spyOn((component as any).router, 'navigate');
+    component.loadReport('variant1');
+    expect(routerSpy).toHaveBeenCalledWith(
+      [
+        'report',
+        { queryParams: {genome: 'hg19', variant: 'variant1'}}
+      ],
+      { relativeTo: (component as any).route }
+    );
   });
 });
--- frontend/src/app/single-annotation/single-annotation.component.ts
+++ frontend/src/app/single-annotation/single-annotation.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { CommonModule } from '@angular/common';
+import { SingleAnnotationService } from '../single-annotation.service';
+import { take } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-single-annotation',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './single-annotation.component.html',
   styleUrl: './single-annotation.component.css'
 })
-export class SingleAnnotationComponent {
+export class SingleAnnotationComponent implements OnInit {
+  public readonly environment = environment;
+  public genomes: string[] = [];
+  public selectedGenome: string = '';
 
+  public constructor(
+    private singleAnnotationService: SingleAnnotationService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
+
+  public ngOnInit(): void {
+    this.singleAnnotationService.getGenomes().pipe(take(1)).subscribe((genomes) => {
+      this.genomes = genomes;
+      this.selectedGenome = genomes[0];
+    });
+  }
+
+  public loadReport(variant: string): void {
+    this.router.navigate(
+      [
+        'report',
+        { queryParams: {genome: this.selectedGenome, variant: variant}}
+      ],
+      { relativeTo: this.route }
+    );
+  }
 }
