c8d33dfe9896f3282b863b6a7706a0c7a2f2fff1
IlinaGergova <ili.gergova@gmail.com>
2025-09-30 14:14:09 +0300

    mock getting genomes in single annotation service

--- frontend/src/app/single-annotation.service.ts
+++ frontend/src/app/single-annotation.service.ts
@@ -7,6 +7,7 @@ import { SingleAnnotationReport } from './single-annotation';
 @Injectable()
 export class SingleAnnotationService {
   private readonly getReportUrl = `${environment.apiPath}`;
+  private readonly getGenomesUrl = `${environment.apiPath}/genomes`;
   public constructor(private http: HttpClient) { }
 
   public getReport(): Observable<SingleAnnotationReport> {
@@ -15,5 +16,9 @@ export class SingleAnnotationService {
       this.getReportUrl,
       options
     ).pipe(map((response: object) => SingleAnnotationReport.fromJson(response)));
+  }
+
+  public getGenomes(): Observable<string[]> {
+    return of(['hg38', 'hg19']);
   }
 }
