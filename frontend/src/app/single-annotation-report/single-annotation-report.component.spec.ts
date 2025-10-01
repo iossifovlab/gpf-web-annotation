d4a32f52f624ba45ff66aed831ac167ede8bbc00
IlinaGergova <ili.gergova@gmail.com>
2025-10-01 11:54:13 +0300

    add unit tests for query params in url

--- frontend/src/app/single-annotation-report/single-annotation-report.component.spec.ts
+++ frontend/src/app/single-annotation-report/single-annotation-report.component.spec.ts
@@ -1,10 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SingleAnnotationReportComponent } from './single-annotation-report.component';
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { Annotator, AnnotatorType, SingleAnnotationReport, Variant } from '../single-annotation';
 import { SingleAnnotationService } from '../single-annotation.service';
-import { provideRouter } from '@angular/router';
+import { ActivatedRoute, provideRouter, Router } from '@angular/router';
 
 const mockReport = new SingleAnnotationReport(
   new Variant('chr14', '204000100', 'A', 'AA', 'ins'),
@@ -13,14 +13,17 @@ const mockReport = new SingleAnnotationReport(
   ],
 );
 class MockSingleAnnotationService {
-  public getReport(): Observable<SingleAnnotationReport> {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public getReport(variant: string, genome: string): Observable<SingleAnnotationReport> {
     return of(mockReport);
   }
 }
+
 describe('SingleAnnotationReportComponent', () => {
   let component: SingleAnnotationReportComponent;
   let fixture: ComponentFixture<SingleAnnotationReportComponent>;
   const mockSingleAnnotationService = new MockSingleAnnotationService();
+  let router: Router;
 
   beforeEach(async() => {
     await TestBed.configureTestingModule({
@@ -34,6 +37,7 @@ describe('SingleAnnotationReportComponent', () => {
       ]
     }).compileComponents();
 
+    router = TestBed.inject(Router);
     fixture = TestBed.createComponent(SingleAnnotationReportComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -46,5 +50,23 @@ describe('SingleAnnotationReportComponent', () => {
   it('should get report from service', () => {
     component.ngOnInit();
     expect(component.report).toBe(mockReport);
+  });
+
+  it('should check if query params from url are passed to get report method', () => {
+    const getReportSpy = jest.spyOn(mockSingleAnnotationService, 'getReport');
+
+    const activatedRoute = TestBed.inject(ActivatedRoute);
+    (activatedRoute.queryParams as BehaviorSubject<{variant: string, genome: string}>).next({
+      variant: 'chr14:204000100:A:AA',
+      genome: 'hg38'
+    });
+    component.ngOnInit();
+    expect(getReportSpy).toHaveBeenCalledWith('chr14:204000100:A:AA', 'hg38');
+  });
+
+  it('should call router.navigate to remove query params after requesting report', () => {
+    const navigateSpy = jest.spyOn(router, 'navigate');
+    component.ngOnInit();
+    expect(navigateSpy).toHaveBeenCalledWith([], { queryParams: {} });
   });
 });
