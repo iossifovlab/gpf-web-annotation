f217b8c97b8c76fa93852b1ff788da546aeb6317
IlinaGergova <ili.gergova@gmail.com>
2025-10-03 13:27:56 +0300

    implement variant validation

--- frontend/src/app/single-annotation/single-annotation.component.spec.ts
+++ frontend/src/app/single-annotation/single-annotation.component.spec.ts
@@ -50,4 +50,58 @@ describe('SingleAnnotationComponent', () => {
       { queryParams: {genome: 'hg19', variant: 'variant1'}, relativeTo: (component as any).route }
     );
   });
+
+  it('should validate variant input', () => {
+    component.validateVariant('chr1 11796321 G A');
+    expect(component.validationMessage).toBe('');
+    component.validateVariant('chr1 GTT A');
+    expect(component.validationMessage).toBe('Invalid variant format!');
+    component.validateVariant('chr1 100 GTT A');
+    expect(component.validationMessage).toBe('');
+    component.validateVariant('chr7 1    GTT A');
+    expect(component.validationMessage).toBe('Invalid variant format!');
+  });
+
+  it('should validate chromosome of a variant for hg19', () => {
+    component.selectedGenome = 'hg19';
+    expect(component.isChromValid('chr1')).toBe(false);
+    expect(component.isChromValid('20')).toBe(true);
+    expect(component.isChromValid('chrX')).toBe(false);
+    expect(component.isChromValid('chr5:148481536')).toBe(false);
+    expect(component.isChromValid('100')).toBe(false);
+    expect(component.isChromValid('aaa')).toBe(false);
+  });
+
+  it('should validate chromosome of a variant for hg38', () => {
+    component.selectedGenome = 'hg38';
+    expect(component.isChromValid('chr1')).toBe(true);
+    expect(component.isChromValid('20')).toBe(false);
+    expect(component.isChromValid('chrX')).toBe(true);
+    expect(component.isChromValid('chr5:148481536')).toBe(false);
+    expect(component.isChromValid('100')).toBe(false);
+    expect(component.isChromValid('aaa')).toBe(false);
+  });
+
+  it('should validate position of a variant', () => {
+    expect(component.isPosValid('11796321')).toBe(true);
+    expect(component.isPosValid('pos:11796321')).toBe(false);
+  });
+
+  it('should validate reference of a variant', () => {
+    expect(component.isRefValid('G')).toBe(true);
+    expect(component.isRefValid('GT')).toBe(true);
+    expect(component.isRefValid('ZZ')).toBe(false);
+    expect(component.isRefValid('GT,N')).toBe(false);
+    expect(component.isRefValid('aaa')).toBe(true);
+    expect(component.isRefValid('')).toBe(false);
+  });
+
+  it('should validate alternative of a variant', () => {
+    expect(component.isAltValid('A')).toBe(true);
+    expect(component.isAltValid('GT')).toBe(true);
+    expect(component.isAltValid('GT,N')).toBe(true);
+    expect(component.isAltValid('gt,a')).toBe(true);
+    expect(component.isAltValid('A,NN,NNP')).toBe(false);
+    expect(component.isAltValid('')).toBe(true);
+  });
 });
--- frontend/src/app/single-annotation/single-annotation.component.ts
+++ frontend/src/app/single-annotation/single-annotation.component.ts
@@ -16,6 +16,7 @@ export class SingleAnnotationComponent implements OnInit {
   public readonly environment = environment;
   public genomes: string[] = [];
   public selectedGenome: string = '';
+  public validationMessage = '';
 
   public constructor(
     private singleAnnotationService: SingleAnnotationService,
@@ -35,5 +36,50 @@ export class SingleAnnotationComponent implements OnInit {
       ['report'],
       { queryParams: { genome: this.selectedGenome, variant: variant }, relativeTo: this.route },
     );
+  }
+
+  public validateVariant(variant: string): void {
+    const v = variant.split(' ');
+    if (
+      v.length === 4 &&
+      this.isChromValid(v[0]) &&
+      this.isPosValid(v[1]) &&
+      this.isRefValid(v[2]) &&
+      this.isAltValid(v[3])
+    ) {
+      this.validationMessage = '';
+      this.loadReport(variant);
+    } else {
+      this.validationMessage = 'Invalid variant format!';
+    }
+  }
+
+  public isChromValid(chromosome: string): boolean {
+    let chromRegex = '(2[0-2]|1[0-9]|[0-9]|X|Y)';
+    if (this.selectedGenome === 'hg38') {
+      chromRegex = 'chr' + chromRegex;
+    }
+    const lineRegex = `${chromRegex}`;
+    const match = chromosome.match(new RegExp(lineRegex, 'i'));
+    return match !== null && match[0] === chromosome;
+  }
+
+  public isPosValid(position: string): boolean {
+    return !isNaN(Number(position));
+  }
+
+  public isRefValid(reference: string): boolean {
+    return reference !== '' && this.areBasesValid(reference);
+  }
+
+  private areBasesValid(bases: string): boolean {
+    const validBases = ['A', 'C', 'G', 'T', 'N', 'a', 'c', 'g', 't', 'n'];
+    const bList = bases.split('');
+    return bList.filter(b => !validBases.includes(b)).length === 0;
+  }
+
+  public isAltValid(alternative: string): boolean {
+    const aList = alternative.split(',');
+    return aList.filter(a => !this.areBasesValid(a)).length === 0;
   }
 }
