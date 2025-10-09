f45f7776b2e34102946956b839ec6f60397f6a37
IlinaGergova <ili.gergova@gmail.com>
2025-10-09 14:40:10 +0300

    add method for requesting histograms in service

--- frontend/src/app/single-annotation.service.ts
+++ frontend/src/app/single-annotation.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { map, Observable, of } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { SingleAnnotationReport, Variant } from './single-annotation';
+import { CategoricalHistogram, NumberHistogram, SingleAnnotationReport, Variant } from './single-annotation';
 import { CookieService } from 'ngx-cookie-service';
 
 
@@ -10,6 +10,7 @@ import { CookieService } from 'ngx-cookie-service';
 export class SingleAnnotationService {
   private readonly getReportUrl = `${environment.apiPath}/single_annotate`;
   private readonly getGenomesUrl = `${environment.apiPath}/genomes`;
+  private readonly getHistogramUrl = `${environment.apiPath}`;
   public constructor(private http: HttpClient, private cookieService: CookieService) { }
 
   public getReport(variant: Variant, genome: string): Observable<SingleAnnotationReport> {
@@ -28,6 +29,20 @@ export class SingleAnnotationService {
       { variant: variantJson, genome: genome },
       options
     ).pipe(map((response: object) => SingleAnnotationReport.fromJson(response)));
+  }
+
+  public getHistogram(histogramUrl: string): Observable<NumberHistogram | CategoricalHistogram> {
+    const csrfToken = this.cookieService.get('csrftoken');
+    const headers = { 'X-CSRFToken': csrfToken };
+    const options = { headers: headers, withCredentials: true };
+
+    return this.http.get<object>(
+      `${this.getHistogramUrl}/${histogramUrl}`,
+      options
+    ).pipe(map((response: object) => {
+      return response['config']['type'] === 'number' ?
+        NumberHistogram.fromJson(response) : CategoricalHistogram.fromJson(response);
+    }));
   }
 
   public getGenomes(): Observable<string[]> {
