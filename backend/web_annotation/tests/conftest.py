390ceaf4a6b3b1c14f26f14782612d716c0c8f49
IvoStefanov <ivostefanovtues@gmail.com>
2025-10-10 10:00:39 +0300

    Setup mailhog client for testing

--- backend/web_annotation/tests/conftest.py
+++ backend/web_annotation/tests/conftest.py
@@ -1,15 +1,17 @@
 import pathlib
 import shutil
 import yaml
-from typing import Generator
 import pytest
 import pytest_mock
 from django.test import Client
 from django.conf import settings, LazySettings
+from typing import Generator, cast
+from urllib.parse import urlparse
 from dae.genomic_resources.repository import GenomicResourceRepo
 from dae.genomic_resources.repository_factory import \
     build_genomic_resource_repository
 
+from web_annotation.tests.mailhog_client import MailhogClient
 from web_annotation.models import Job, User
 
 
@@ -22,6 +24,27 @@ def use_test_grr_definition(
     grr_path = tmp_path / "grr_definition.yaml"
     grr_path.write_text(yaml.safe_dump(test_grr.definition))
     settings.GRR_DEFINITION = grr_path
+
+def pytest_addoption(parser: pytest.Parser) -> None:
+    parser.addoption(
+        "--url",
+        dest="url",
+        action="store",
+        default="http://localhost:21011",
+        help="REST API URL",
+    )
+
+    parser.addoption(
+        "--mailhog",
+        dest="mailhog",
+        action="store",
+        default="http://localhost:8025",
+        help="Mailhog REST API URL",
+    )
+
+
+@pytest.fixture(scope="function")
+def test_grr(mocker: pytest_mock.MockFixture) -> GenomicResourceRepo:
     grr_patch = \
         mocker.patch("web_annotation.views.AnnotationBaseView.get_grr")
 
@@ -109,3 +132,28 @@ def user_client() -> Client:
     client = Client()
     client.login(email="user@example.com", password="secret")
     return client
+
+@pytest.fixture
+def anonymous_client() -> Client:
+    return Client()
+
+@pytest.fixture
+def mail_client(mailhog_url: str, settings: LazySettings) -> MailhogClient:
+    """REST client fixture."""
+    # Workaround for django test environment setup being hardcoded to
+    # always set up a locmem backend
+    settings.EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    return MailhogClient(mailhog_url)
+
+@pytest.fixture
+def mailhog_url(request: pytest.FixtureRequest) -> str:
+    """Mailhog URL fixture."""
+    res = cast(str, request.config.getoption("--mailhog"))
+    parsed = urlparse(res)
+    if not parsed.scheme:
+        res = f"http://{res}"
+    if parsed.scheme not in {"http", "https"}:
+        raise ValueError(f"Invalid URL: {res}")
+    parsed = urlparse(res)
+    path = parsed.path.rstrip("/")
+    return f"{parsed.scheme}://{parsed.netloc}{path}"
