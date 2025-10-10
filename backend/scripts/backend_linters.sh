3ab96d52e3d4fdf419cd546160f563885ca7d9a2
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2025-10-10 13:35:47 +0300

    backend tests results are stored in backend/results

--- backend/scripts/backend_linters.sh
+++ backend/scripts/backend_linters.sh
@@ -4,18 +4,19 @@
     pip install --root-user-action ignore -e /wd/backend
 
 cd /wd/
+mkdir -p /wd/backend/results
 
 /opt/conda/bin/conda run --no-capture-output -n gpf ruff check \
     --exit-zero \
     --output-format=pylint \
-    --output-file=/wd/results/ruff_report backend/web_annotation || true
+    --output-file=/wd/backend/results/ruff_report backend/web_annotation || true
 
 /opt/conda/bin/conda run --no-capture-output -n gpf \
     pylint backend/web_annotation -f parseable --reports=no -j 4 \
-    --exit-zero > /wd/results/pylint_report || true
+    --exit-zero > /wd/backend/results/pylint_report || true
 
 /opt/conda/bin/conda run --no-capture-output -n gpf mypy \
     backend/web_annotation \
     --pretty \
     --show-error-context \
-    --no-incremental > /wd/results/mypy_report || true
+    --no-incremental > /wd/backend/results/mypy_report || true
--- backend/scripts/backend_tests.sh
+++ backend/scripts/backend_tests.sh
@@ -20,19 +20,20 @@ while true; do
 done
 
 cd /wd/backend/
+mkdir -p /wd/backend/results
 
 /opt/conda/bin/conda run --no-capture-output -n gpf \
     py.test -v web_annotation/tests \
         --cov-config /wd/backend/coveragerc \
         --cov web_annotation \
-        --junitxml=/wd/results/backend-tests-junit.xml \
+        --junitxml=/wd/backend/results/backend-tests-junit.xml \
         --mailhog http://mail:8025
 
 /opt/conda/bin/conda run -n gpf \
     coverage xml
 
 /opt/conda/bin/conda run -n gpf \
-    coverage html --title web_annotation -d /wd/results/coverage-html
+    coverage html --title web_annotation -d /wd/backend/results/coverage-html
 
 
-cp /wd/backend/coverage.xml /wd/results/backend-coverage.xml
+cp /wd/backend/coverage.xml /wd/backend/results/backend-coverage.xml
