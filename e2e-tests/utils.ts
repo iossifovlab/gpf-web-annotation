93f7b990677ac16683657b43f1f4669720540764
IvoStefanov <ivostefanovtues@gmail.com>
2025-10-16 15:12:44 +0300

    Update e2e tests to register users succesfuly
    Activate users through the sent emails to activate the user accounts.

--- e2e-tests/utils.ts
+++ e2e-tests/utils.ts
@@ -1,5 +1,8 @@
 import { expect, Page } from '@playwright/test';
 
+export const mailhogUrl = 'http://mail:8025';
+// export const mailhogUrl = 'http://localhost:8025';
+
 export function getRandomString(): string {
   return Math.random().toString(36).substring(2, 9);
 }
@@ -9,6 +12,15 @@ export async function registerUser(page: Page, email: string, password: string):
   await page.locator('#email').pressSequentially(email);
   await page.locator('#password').pressSequentially(password);
   await page.getByRole('button', { name: 'Create' }).click();
+
+  await page.goto(mailhogUrl, {waitUntil: 'load'});
+  await page.getByText(email).click();
+  const href = await page.locator('#preview-plain > a').getAttribute('href');
+  if (!href) {
+    throw new Error('Confirmation link not found in email.');
+  }
+  await page.goto(href, {waitUntil: 'load'});
+
   await expect(page.locator('app-login')).toBeVisible();
 }
 
