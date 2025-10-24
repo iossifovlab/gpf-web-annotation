09f1d6913e9f6363130c7162767e83af68f7a9bc
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2025-10-24 12:19:11 +0300

    Clean up lint error in models.py

--- backend/web_annotation/models.py
+++ backend/web_annotation/models.py
@@ -47,7 +47,7 @@ class Job(models.Model):
         path=settings.ANNOTATION_CONFIG_STORAGE_DIR)
     result_path = models.FilePathField(
         path=settings.JOB_RESULT_STORAGE_DIR)
-    reference_genome=models.CharField(max_length=1024, default="")
+    reference_genome = models.CharField(max_length=1024, default="")
     created = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=Status, default=Status.WAITING)
 
