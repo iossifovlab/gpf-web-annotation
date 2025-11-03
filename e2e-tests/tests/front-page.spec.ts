eed149e953065cd2b845fadf4bb5adc65930d201
IlinaGergova <ili.gergova@gmail.com>
2025-11-03 14:38:12 +0200

    update front page e2e tests

--- e2e-tests/tests/front-page.spec.ts
+++ e2e-tests/tests/front-page.spec.ts
@@ -3,44 +3,39 @@ import * as utils from '../utils';
 
 
 test.describe('Basic tests', () => {
-  test.beforeEach(async({ page }) => {
+  test('should check if single annotation page is loaded by default', async({ page }) => {
     await page.goto('/', {waitUntil: 'load'});
+    expect(page.url()).toContain('/single-annotation');
   });
 
-  test('should check if login page is loaded by default', ({ page }) => {
-    expect(page.url()).toContain('/login');
-  });
-
-  test('should check if user is redirected to login page after ' +
+  test('should check if user is redirected to default page after ' +
       'trying to access page without being logged in', async({ page }) => {
     await page.goto('/jobs', {waitUntil: 'load'});
-    await expect(page.locator('app-login')).toBeVisible();
-    expect(page.url()).toContain('/login');
+    await expect(page.locator('app-single-annotation')).toBeVisible();
+    expect(page.url()).toContain('/single-annotation');
   });
 
   test('should check visible elements of login page', async({ page }) => {
+    await page.goto('/login', {waitUntil: 'load'});
     await expect(page.locator('#front-page-container')).toBeVisible();
     await expect(page.locator('#login-container')).toBeVisible();
     await expect(page.locator('#email')).toBeVisible();
     await expect(page.locator('#password')).toBeVisible();
     await expect(page.locator('#register-link')).toBeVisible();
-    await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
+    await expect(page.locator('#login-container').getByRole('button', { name: 'Login' })).toBeVisible();
     await expect(page.locator('#divider')).toBeVisible();
     await expect(page.getByRole('button', { name: 'Continue with Google'})).toBeVisible();
   });
 
   test('should check application description', async({ page }) => {
+    await page.goto('/login', {waitUntil: 'load'});
     await expect(page.locator('#front-page-header p')).toHaveText('GPF Web Annotation description');
   });
 
   test('should check url after navigating to register page from login page', async({ page }) => {
+    await page.goto('/login', {waitUntil: 'load'});
     await page.locator('#register-link').click();
     expect(page.url()).toContain('/register');
-  });
-
-  test('should check if user is redirected to register page after clicking the link in login page', async({ page }) => {
-    await page.locator('#register-link').click();
-    await expect(page.locator('app-registration')).toBeVisible();
   });
 
   test('should check if user is redirected to login page after clicking the link in register page', async({ page }) => {
@@ -53,8 +48,7 @@ test.describe('Basic tests', () => {
 
 test.describe('Registration tests', () => {
   test.beforeEach(async({ page }) => {
-    await page.goto('/', {waitUntil: 'load'});
-    await page.locator('#register-link').click();
+    await page.goto('/register');
   });
 
   test('should successfully create user', async({ page }) => {
@@ -108,14 +102,14 @@ test.describe('Registration tests', () => {
 
     await page.locator('#email').pressSequentially(randomEmail);
     await page.locator('#password').pressSequentially('password123');
-    await page.getByRole('button', { name: 'Login' }).click();
+    await page.locator('#login-container').getByRole('button', { name: 'Login' }).click();
     await expect(page.locator('app-single-annotation')).toBeVisible();
   });
 });
 
 test.describe('Login tests', () => {
   test.beforeEach(async({ page }) => {
-    await page.goto('/', {waitUntil: 'load'});
+    await page.goto('/login', {waitUntil: 'load'});
   });
 
   test('should check if jobs table page is visible after successful login', async({ page }) => {
@@ -124,42 +118,42 @@ test.describe('Login tests', () => {
 
     await page.locator('#email').pressSequentially(randomEmail);
     await page.locator('#password').pressSequentially('password123');
-    await page.getByRole('button', { name: 'Login' }).click();
+    await page.locator('#login-container').getByRole('button', { name: 'Login' }).click();
     await expect(page.locator('app-single-annotation')).toBeVisible();
     await expect(page.locator('#user-data')).toBeVisible();
   });
 
-  test('should check if user is redirected to login page after logout', async({ page }) => {
+  test('should check if user is redirected to default page after logout', async({ page }) => {
     const randomEmail = `${utils.getRandomString()}@email.com`;
     await utils.registerUser(page, randomEmail, 'password123');
 
     await page.locator('#email').pressSequentially(randomEmail);
     await page.locator('#password').pressSequentially('password123');
-    await page.getByRole('button', { name: 'Login' }).click();
+    await page.locator('#login-container').getByRole('button', { name: 'Login' }).click();
     await page.waitForSelector('app-single-annotation');
 
     await page.getByRole('button', { name: 'Logout' }).click();
-    await expect(page.locator('app-login')).toBeVisible();
+    await expect(page.locator('app-single-annotation')).toBeVisible();
   });
 
   test('should show error message when trying to login with invalid credentials', async({ page }) => {
     await page.locator('#email').pressSequentially('nonexistent@email.com');
     await page.locator('#password').pressSequentially('nonexistentpassword');
-    await page.getByRole('button', { name: 'Login' }).click();
+    await page.locator('#login-container').getByRole('button', { name: 'Login' }).click();
     await expect(page.getByText('Invalid login credentials')).toBeVisible();
     await expect(page.locator('app-single-annotation')).not.toBeVisible();
   });
 
   test('should show error message when trying to login without email', async({ page }) => {
     await page.locator('#password').pressSequentially('nonexistentpassword');
-    await page.getByRole('button', { name: 'Login' }).click();
+    await page.locator('#login-container').getByRole('button', { name: 'Login' }).click();
     await expect(page.getByText('Invalid login credentials')).toBeVisible();
     await expect(page.locator('app-single-annotation')).not.toBeVisible();
   });
 
   test('should show error message when trying to login without password', async({ page }) => {
     await page.locator('#email').pressSequentially('nonexistent@email.com');
-    await page.getByRole('button', { name: 'Login' }).click();
+    await page.locator('#login-container').getByRole('button', { name: 'Login' }).click();
     await expect(page.getByText('Invalid login credentials')).toBeVisible();
     await expect(page.locator('app-single-annotation')).not.toBeVisible();
   });
@@ -170,10 +164,11 @@ test.describe('Login tests', () => {
 
     await page.locator('#email').pressSequentially(randomEmail);
     await page.locator('#password').pressSequentially('password123');
-    await page.getByRole('button', { name: 'Login' }).click();
+    await page.locator('#login-container').getByRole('button', { name: 'Login' }).click();
     await expect(page.locator('app-single-annotation')).toBeVisible();
 
     await page.locator('#logout-button').click();
+    await page.goto('/login', {waitUntil: 'load'});
     await page.locator('#reset-password-link').click();
     await page.locator('#id_email').pressSequentially(randomEmail);
     await page.locator('input[value="Reset password"]').click();
@@ -189,7 +184,7 @@ test.describe('Login tests', () => {
 
     await page.locator('#email').pressSequentially(randomEmail);
     await page.locator('#password').pressSequentially(newPassword);
-    await page.getByRole('button', { name: 'Login' }).click();
+    await page.locator('#login-container').getByRole('button', { name: 'Login' }).click();
     await expect(page.locator('app-single-annotation')).toBeVisible();
     await expect(page.locator('#user-data')).toBeVisible();
   });
@@ -197,7 +192,7 @@ test.describe('Login tests', () => {
 
 test.describe('Logout tests', () => {
   test.beforeEach(async({ page }) => {
-    await page.goto('/', {waitUntil: 'load'});
+    await page.goto('/login', {waitUntil: 'load'});
 
     const email = utils.getRandomString() + '@email.com';
     const password = 'aaabbb';
@@ -206,31 +201,32 @@ test.describe('Logout tests', () => {
     await utils.loginUser(page, email, password);
   });
 
-  test('should redirect to login page after logout', async({ page }) => {
+  test('should redirect to single annotation page after logout', async({ page }) => {
     await page.locator('#logout-button').click();
-    await expect(page.locator('#front-page-container')).toBeVisible();
-    expect(page.url()).toContain('/login');
+    await expect(page.locator('app-single-annotation')).toBeVisible();
+    expect(page.url()).toContain('/single-annotation');
   });
 
   test('should stay logged out after refreshing the page', async({ page }) => {
     await page.locator('#logout-button').click();
-    await page.waitForSelector('#front-page-container');
+    await page.waitForSelector('app-single-annotation');
     await page.reload();
-    await expect(page.locator('#front-page-container')).toBeVisible();
+    await expect(page.locator('app-single-annotation')).toBeVisible();
   });
 
   test('should stay logged out after clicking back button', async({ page }) => {
+    await page.getByRole('link', {name: 'Annotation Jobs'}).click();
     await page.locator('#logout-button').click();
-    await page.waitForSelector('#front-page-container');
+    await page.waitForSelector('app-single-annotation');
     await page.goBack();
-    await expect(page.locator('#front-page-container')).toBeVisible();
+    await expect(page.locator('app-single-annotation')).toBeVisible();
   });
 
   test('should not be able to navigate to jobs table page after logout', async({ page }) => {
     await page.locator('#logout-button').click();
-    await page.waitForSelector('#front-page-container');
+    await page.waitForSelector('app-single-annotation');
     await page.goto('/jobs', {waitUntil: 'load'});
-    await expect(page.locator('#front-page-container')).toBeVisible();
-    expect(page.url()).toContain('/login');
+    await expect(page.locator('app-single-annotation')).toBeVisible();
+    expect(page.url()).toContain('/single-annotation');
   });
 });
