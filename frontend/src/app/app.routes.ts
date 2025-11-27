9fe8f03c5e577dc7b24f14ff3b1f1ce0d7959c9f
IlinaGergova <ili.gergova@gmail.com>
2025-11-27 15:16:57 +0200

    remove auth guard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
-import { authGuard } from './auth-guard';
 import { AnnotationWrapperComponent } from './annotation-wrapper/annotation-wrapper.component';
 
 export const routes: Routes = [
-  { path: '', component: AnnotationWrapperComponent, canActivate: [authGuard], pathMatch: 'full' },
+  { path: '', component: AnnotationWrapperComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegistrationComponent },
 ];
