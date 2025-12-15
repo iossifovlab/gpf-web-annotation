7be610dc9d5997eb0602825dfee09798d06b3d08
IlinaGergova <ili.gergova@gmail.com>
2025-12-15 14:17:42 +0200

    fix editor resizing to min and max

--- frontend/src/app/annotation-pipeline/annotation-pipeline.component.ts
+++ frontend/src/app/annotation-pipeline/annotation-pipeline.component.ts
@@ -54,7 +54,7 @@ export class AnnotationPipelineComponent implements OnInit, OnDestroy, AfterView
   public yamlEditorOptions = {};
   public displayFullScreenButton = true;
   public displayResetScreenButton = false;
-  public setEditorMaxSize = false;
+  public editorSize: 'small' | 'full' | 'custom' = 'small';
   @Output() public tiggerHidingComponents = new EventEmitter<boolean>();
   public isUserLoggedIn = false;
   public lastNotification: PipelineNotification = null;
@@ -131,17 +131,16 @@ export class AnnotationPipelineComponent implements OnInit, OnDestroy, AfterView
     if (editorElement.clientWidth < maxWidth && editorElement.clientWidth > minWidth) {
       this.displayFullScreenButton = true;
       this.displayResetScreenButton = true;
+      this.editorSize = 'custom';
     }
   }
 
   private resolveComponentsVisibility(editorElement: HTMLElement): void {
     const parentWidth = document.getElementById('annotation-container').clientWidth;
-    if (!this.isEditorMaximized(editorElement)) {
-      if (editorElement.clientWidth > parentWidth/1.5) {
-        this.hideParentComponents();
-      } else if (editorElement.clientWidth < parentWidth/1.5) {
-        this.showParentComponents();
-      }
+    if (editorElement.clientWidth > parentWidth/1.5) {
+      this.hideParentComponents();
+    } else if (editorElement.clientWidth < parentWidth/1.5) {
+      this.showParentComponents();
     }
     this.resolveResizeButtonsVisibility(editorElement);
   }
@@ -284,14 +283,18 @@ export class AnnotationPipelineComponent implements OnInit, OnDestroy, AfterView
   }
 
   public expandTextarea(): void {
-    this.isFullScreen = true;
-    this.setEditorMaxSize = true;
+    this.displayFullScreenButton = false;
+    this.displayResetScreenButton = true;
+    this.editorSize = 'full';
+
     this.hideParentComponents();
   }
 
   public shrinkTextarea(): void {
-    this.isFullScreen = false;
-    this.setEditorMaxSize = false;
+    this.displayFullScreenButton = true;
+    this.displayResetScreenButton = false;
+    this.editorSize = 'small';
+
     this.showParentComponents();
   }
 
@@ -307,6 +310,26 @@ export class AnnotationPipelineComponent implements OnInit, OnDestroy, AfterView
     this.socketNotificationsService.closeConnection();
     if (this.resizeObserver) {
       this.resizeObserver.disconnect();
+    }
+  }
+
+  public editorWidth(): string {
+    if (this.editorSize === 'full') {
+      return '95vw';
+    } else if (this.editorSize === 'small') {
+      return '40vw';
+    } else {
+      return 'auto';
+    }
+  }
+
+  public editorHeight(): string {
+    if (this.editorSize === 'full') {
+      return '70vh';
+    } else if (this.editorSize === 'small') {
+      return '40vh';
+    } else {
+      return 'auto';
     }
   }
 }
