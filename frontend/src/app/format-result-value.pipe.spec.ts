c5af823bbde27747a54bcb5492f897aaf5ca29d0
IlinaGergova <ili.gergova@gmail.com>
2025-12-19 11:55:34 +0200

    format numbers using Python-style .g formatter
    * add unit tests

--- frontend/src/app/format-result-value.pipe.spec.ts
+++ frontend/src/app/format-result-value.pipe.spec.ts
@@ -5,4 +5,35 @@ describe('FormatResultValuePipe', () => {
     const pipe = new FormatResultValuePipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should format string value', () => {
+    const pipe = new FormatResultValuePipe();
+    expect(pipe.transform('missense')).toBe('missense');
+    expect(pipe.transform(
+      'ENST00000376583.7:MTHFR:missense:263/697(Ala->Val)|ENST00000376585.6:MTHFR:missense:263/697(Ala->Val)'
+    )).toBe('ENST00000376583.7:MTHFR:missense:263/697(Ala->Val)|ENST00000376585.6:MTHFR:missense:263/697(Ala->Val)');
+  });
+
+  it('should format number value', () => {
+    const pipe = new FormatResultValuePipe();
+    expect(pipe.transform(0, 3)).toBe('0');
+    expect(pipe.transform(10, 3)).toBe('10');
+    expect(pipe.transform(123, 3)).toBe('123');
+    expect(pipe.transform(1234, 3)).toBe('1.23e+03');
+    expect(pipe.transform(1234, 4)).toBe('1234');
+    expect(pipe.transform(1801133, 3)).toBe('1.8e+06');
+    expect(pipe.transform(0.32269200682640076, 3)).toBe('0.323');
+  });
+
+  it('should format value of type map with strings', () => {
+    const pipe = new FormatResultValuePipe();
+    const map = new Map<string, string>([['MTHFR', 'missense'], ['ABC', 'nonsense']]);
+    expect(pipe.transform(map)).toBe('MTHFR:missense; ABC:nonsense; ');
+  });
+
+  it('should format value of type map with string and number', () => {
+    const pipe = new FormatResultValuePipe();
+    const map = new Map<string, number>([['MTHFR', 15454], ['ABC', 52]]);
+    expect(pipe.transform(map, 3)).toBe('MTHFR:1.55e+04; ABC:52; ');
+  });
 });
--- frontend/src/app/format-result-value.pipe.ts
+++ frontend/src/app/format-result-value.pipe.ts
@@ -4,17 +4,39 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'formatResultValue'
 })
 export class FormatResultValuePipe implements PipeTransform {
-  public transform(value: string | number | Map<string, string | number>): string {
+  public transform(value: string | number | Map<string, string | number>, numberOfDigits?: number): string {
     let result = '';
     if (value instanceof Map) {
       value.forEach((v, k) => {
-        result += `${k}:${v}; `;
+        if (typeof v === 'number') {
+          result += `${k}:${this.formatNumber(v, numberOfDigits)}; `;
+        } else {
+          result += `${k}:${v}; `;
+        }
       });
     } if (typeof value === 'number') {
-      result = value === 0 ? '0' : value.toPrecision(3);
+      result = this.formatNumber(value, numberOfDigits);
     } else if (typeof value === 'string') {
       return value;
     }
     return result;
+  }
+
+  // python format
+  private formatNumber(value: number, numberOfDigits: number): string {
+    if (value.toString().length < numberOfDigits) {
+      return value.toString();
+    }
+
+    let s = value.toPrecision(numberOfDigits);
+
+    // Remove trailing zeros in mantissa
+    s = s.replace(/(\.\d*?[1-9])0+e/, '$1e');
+    s = s.replace(/\.0+e/, 'e');
+
+    // Pad exponent to two digits
+    s = s.replace(/e([+-])(\d)$/, 'e$10$2');
+
+    return s;
   }
 }
