f93cdf5a9c0fcca78e65404664f87c8ad0e2cc33
IlinaGergova <ili.gergova@gmail.com>
2026-01-05 16:02:32 +0200

    add alleles table component jest unit tests

--- frontend/src/app/alleles-table/alleles-table.component.spec.ts
+++ frontend/src/app/alleles-table/alleles-table.component.spec.ts
@@ -2,10 +2,18 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AllelesTableComponent } from './alleles-table.component';
 import { SingleAnnotationService } from '../single-annotation.service';
 import { Observable, of } from 'rxjs';
+import { cloneDeep } from 'lodash';
 
+
+const mockAlleleHistory = ['chr1 11777321 G A', 'chr1 11999921 G TT'];
 class SingleAnnotationServiceMock {
   public getAllelesHistory(): Observable<string[]> {
-    return of(['chr1 11796321 G A', 'chr1 11796321 G TT']);
+    return of(cloneDeep(mockAlleleHistory));
+  }
+
+  public deleteAllele(alleleId: number): Observable<object> {
+    mockAlleleHistory.splice(alleleId, 1);
+    return of({});
   }
 }
 
@@ -24,10 +32,36 @@ describe('AllelesTableComponent', () => {
 
     fixture = TestBed.createComponent(AllelesTableComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should request alleles history on init', () => {
+    const getAllelesSpy = jest.spyOn(singleAnnotationServiceMock, 'getAllelesHistory');
+    component.ngOnInit();
+    expect(getAllelesSpy).toHaveBeenCalledWith();
+    expect(component.allelesHistory).toStrictEqual(['chr1 11999921 G TT', 'chr1 11777321 G A']);
+  });
+
+  it('should refresh history table on init', () => {
+    const getAllelesSpy = jest.spyOn(singleAnnotationServiceMock, 'getAllelesHistory');
+    component.ngOnInit();
+    expect(getAllelesSpy).toHaveBeenCalledWith();
+    expect(component.allelesHistory).toStrictEqual(['chr1 11999921 G TT', 'chr1 11777321 G A']);
+  });
+
+  it('should delete allele by id from history table', () => {
+    const getAllelesSpy = jest.spyOn(singleAnnotationServiceMock, 'getAllelesHistory');
+    component.onDelete(0);
+    expect(getAllelesSpy).toHaveBeenCalledWith();
+    expect(component.allelesHistory).toStrictEqual(['chr1 11999921 G TT']);
+  });
+
+  it('should trigger request by clicking on allele', () => {
+    const emitSpy = jest.spyOn(component.emitAllele, 'emit');
+    component.makeRequest(mockAlleleHistory[1]);
+    expect(emitSpy).toHaveBeenCalledWith(mockAlleleHistory[1]);
   });
 });
