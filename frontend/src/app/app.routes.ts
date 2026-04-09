562b13f5bd416a1f8716b27c4478173cea1e9a0b
IlinaGergova <ili.gergova@gmail.com>
2026-04-09 12:19:18 +0300

    Enable navgation to annotation components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,9 +3,13 @@ import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { AnnotationJobsWrapperComponent } from './annotation-jobs-wrapper/annotation-jobs-wrapper.component';
 import { AboutPageComponent } from './about-page/about-page.component';
+import {
+  SingleAlleleAnnotationWrapperComponent
+} from './single-allele-annotation-wrapper/single-allele-annotation-wrapper.component';
 
 export const routes: Routes = [
-  { path: '', component: AnnotationJobsWrapperComponent },
+  { path: 'annotation-jobs', component: AnnotationJobsWrapperComponent },
+  { path: 'single-allele-annotation', component: SingleAlleleAnnotationWrapperComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegistrationComponent },
   { path: 'about', component: AboutPageComponent },
